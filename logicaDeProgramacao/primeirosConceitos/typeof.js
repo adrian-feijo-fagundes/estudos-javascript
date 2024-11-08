752188e936ea511f7f43155982a97f7242555e4f
Adrian Fagundes <adrianfeijofagundes@gmail.com>
2024-11-08 06:35:35 -0300

    "CREATE: `typeof.js`"

---
+++ logicaDeProgramacao/primeirosConceitos/typeof.js
@@ -0,0 +1,13 @@
+// Uso do typeof para verificar o tipo de dado
+const nome = "Adrian";
+console.log(typeof nome); // string
+
+// Exemplos de uso do typeof em valores literais e variáveis
+console.log('Exemplo de outro uso de typeof');
+console.log(typeof 202);             // number
+console.log(typeof (200 == 100));    // boolean
+console.log(typeof (() => {}));      // function
+console.log(typeof []);              // object
+console.log(typeof {});              // object
+console.log(typeof null);            // object
+console.log(typeof '');              // string
