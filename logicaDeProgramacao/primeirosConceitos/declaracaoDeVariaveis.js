e02a017bbdae0ca1cba328ace537fa278ac10ab6
Adrian Fagundes <adrianfeijofagundes@gmail.com>
2024-11-08 06:33:46 -0300

    "CREATE: `declaracaoDeVariaveis`"

---
+++ logicaDeProgramacao/primeirosConceitos/declaracaoDeVariaveis.js
@@ -0,0 +1,26 @@
+// Declaração de variáveis
+
+
+// Variáveis constantes (const) - Não podem ter o valor alterado
+const nome = "Adrian";
+const sobrenome = "Fagundes";
+const souEstudante = true;
+
+
+// Variáveis mutáveis (let) - Podem ter o valor alterado
+let idade = 21;
+
+
+// Convenção de nomes de variáveis
+const camelCase = true;
+const snake_case = true;
+
+
+
+
+
+// Comentário em uma linha
+
+/*
+    Comentário em mais de uma linha
+*/
